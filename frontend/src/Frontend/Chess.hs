e667abb6e1d1c75fc50ecaea71ac4d9371ee2400
Jimmy Hartzell <jimmy.hartzell@obsidian.systems>
2019-05-25 22:43:09 -0400

    supports many of chess moves and rules

--- frontend/src/Frontend/Chess.hs
+++ frontend/src/Frontend/Chess.hs
@@ -3,14 +3,18 @@
 {-# LANGUAGE RankNTypes        #-}
 {-# LANGUAGE RecursiveDo       #-}
 {-# LANGUAGE TemplateHaskell   #-}
+{-# LANGUAGE LambdaCase   #-}
 {-# LANGUAGE TypeApplications      #-}
 {-# LANGUAGE RankNTypes            #-}
 {-# LANGUAGE DataKinds             #-}
 
 module Frontend.Chess (app) where
 
+import Control.Monad
 import Control.Monad.Fix (MonadFix)
 import Data.Array.IArray as A
+import Data.Maybe
+import qualified Data.Text as T
 
 import Reflex.Dom
 import Obelisk.Generated.Static
@@ -31,7 +35,8 @@ instance Show ColoredPiece where
 type Board = Array Point (Maybe ColoredPiece)
 
 data GameState = GameState 
-  { board :: Board
+  { board       :: Board
+  , turn        :: Color
   , startOfMove :: Maybe Point
   }
 
@@ -39,9 +44,9 @@ initialBoard :: Board
 initialBoard = A.array ((0, 0), (7,7))
                [ ((i, j), square i j) | i <- [0..7], j <- [0..7]]
   where
-    color n | n >= 6 = Just Black
-            | n <= 1 = Just White
-            | otherwise = Nothing
+    initialColor n | n >= 6 = Just Black
+                   | n <= 1 = Just White
+                   | otherwise = Nothing
     mainRow n = n == 7 || n == 0
     pawnRow n = n == 1 || n == 6
     piece i j | pawnRow j = Just Pawn
@@ -55,10 +60,10 @@ initialBoard = A.array ((0, 0), (7,7))
                 6 -> Just Knight
                 7 -> Just Rook
               | otherwise = Nothing
-    square i j = ColoredPiece <$> color j <*> piece i j
+    square i j = ColoredPiece <$> initialColor j <*> piece i j
 
 initialState :: GameState
-initialState = GameState initialBoard Nothing
+initialState = GameState initialBoard White Nothing
 
 mkBoard
   :: ( DomBuilder t m
@@ -90,14 +95,16 @@ cell
      )
   => Dynamic t GameState -> Point -> m (Event t Point)
 cell gs p = el "td" $ do
-    (e, _) <- elDynAttr' "img" (fmap (square p . board) gs) (return ())
+    (e, _) <- elDynAttr' "img" (square p <$> gs) (return ())
     return $ p <$ domEvent Click e
     where
-      square pos bd = "src" =: (translate $ bd A.! p)
-        <> "style" =: ("display: block; width: 45px; height: 45px; background-color: " <> backgroundColor p)
+      square pos (GameState bd _ active) = "src" =: (translate $ bd A.! p)
+        <> "style" =: ("display: block; width: 45px; height: 45px; background-color: " <> backgroundColor active p)
         <> "draggable" =: "false"
-      backgroundColor (i,j) | (i + j) `mod` 2 == 0 = "grey"
-                            | otherwise            = "white"
+      backgroundColor (Just p) p' | p == p' = "yellow"
+      backgroundColor _ p                   = defaultColor p
+      defaultColor (i,j) | (i + j) `mod` 2 == 0 = "grey"
+                         | otherwise            = "white"
       translate (Just (ColoredPiece clr p)) = translatePiece clr p
       translate _ = static @"chess/blank.svg"
       translatePiece White King   = static @"chess/kl.svg"
@@ -114,7 +121,88 @@ cell gs p = el "td" $ do
       translatePiece Black Pawn   = static @"chess/pd.svg"
 
 move :: Point -> GameState -> GameState
-move _ gs = gs -- XXX for right now
+move new gs@(GameState brd clr act) = fromMaybe (game clr brd) $ case act of
+  Nothing -> do
+    Just (ColoredPiece color piece) <- pure $ brd A.! new
+    guard $ color == clr
+    pure $ GameState brd clr $ Just new
+  Just old -> do
+    guard $ validBasicMove brd clr old new
+    let newBoard = brd // [(old, Nothing), (new, brd A.! old)]
+    guard $ not $ inCheck newBoard clr
+    pure $ game (opponent clr) newBoard
+  where
+    game c b = GameState b c Nothing
+    validGameMove old = validBasicMove brd clr old new
+    validGrab = isJust $ do
+      Just (ColoredPiece color piece) <- pure $ brd A.! new
+      guard $ color == clr
+
+inCheck :: Board -> Color -> Bool
+inCheck board turn = not $ null $ do
+  let points = [(i, j) | i <- [0..7], j <- [0..7]]
+
+  new <- points
+  Just (ColoredPiece newColor King) <- pure $ board A.! new
+  guard $ newColor == turn
+
+  old <- points
+  let attacker = opponent turn
+  guard $ validBasicMove board attacker old new
+
+opponent :: Color -> Color
+opponent = \case
+  Black -> White
+  White -> Black
+
+validBasicMove :: Board -> Color -> Point -> Point -> Bool
+validBasicMove brd turn old new = isJust $ do
+  Just (ColoredPiece color piece) <- pure $ brd A.! old
+  guard $ turn == color
+  guard $ old /= new
+  let dest = brd A.! new
+
+  isCapture <- case dest of
+    Just (ColoredPiece color _) -> do
+      guard $ color /= turn
+      pure True
+    Nothing -> pure False
+
+  let ((oldX, oldY), (newX, newY)) = (old, new)
+      diffX = newX - oldX
+      diffY = newY - oldY
+
+      rookLike = diffX == 0 || diffY == 0
+      bishopLike = abs diffX == abs diffY
+      pawnLike = diffX == 0 && (diffY == pawnDirection || diffY == 2 * pawnDirection && oldY == pawnRow)
+      pawnCaptureLike = diffY == pawnDirection && abs diffX == 1
+      knightLike = (abs diffX == 1 && abs diffY == 2) || (abs diffX == 2 && abs diffY == 1)
+      kingLike = abs diffX <= 1 && abs diffY <= 1
+
+      -- There has got to be a better way to write this:
+      stepX = signum diffX
+      stepY = signum diffY
+      steps (x, y) | x >= 8 || y >= 8       = []
+                   | x == newX && y == newY = []
+                   | otherwise              = (x, y):steps (x + stepX, y + stepY)
+      clearPath = all isNothing $ (brd A.!) <$> steps (oldX + stepX, oldY + stepY)
+
+  case piece of
+    Pawn -> guard $ if isCapture then pawnCaptureLike else pawnLike && clearPath
+    Bishop -> guard $ bishopLike && clearPath
+    Rook -> guard $ rookLike && clearPath
+    Queen -> guard $ (rookLike || bishopLike) && clearPath
+    Knight -> guard knightLike
+    King -> guard kingLike
+
+  pure ()
+  where
+    pawnDirection = case turn of
+      White -> 1
+      Black -> -1
+    pawnRow = case turn of
+      White -> 1
+      Black -> 6
 
 app
   :: ( DomBuilder t m
@@ -132,4 +220,12 @@ app = divClass "container" $ do
       rec
         gs  <- foldDyn move initialState pos
         pos <- mkBoard gs
-      return ()
+        elAttr "h3" ("style" =: "text-align: center") $ do
+          dynText $ T.pack . show . turn <$> gs
+          text "'s Turn"
+          dynText $ checkText . checkInCheck <$> gs
+      pure ()
+  where
+  checkText True = " ... and they're in check"
+  checkText False = ""
+  checkInCheck (GameState board turn _) = inCheck board turn
